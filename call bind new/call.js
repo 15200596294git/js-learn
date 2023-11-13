b4fd85ac64b43b379c1482680ab343f319bf7694
lwj <jg15575446613@163.com>
2023-11-13 18:07:05 +0800

    chore: call学习

--- call bind new/call.js
+++ call bind new/call.js
@@ -3,16 +3,20 @@ var foo = {
 
 }
 
-function func() {
+function func(a,b) {
+  console.log("🚀 ~ file: call.js:7 ~ a:", a)
+
+  console.log("🚀 ~ file: call.js:9 ~ b:", b)
+
   // this.value
-  console.log("🚀 ~ file: call.js:8 ~ this.value:", this.value)
+  // console.log("🚀 ~ file: call.js:8 ~ this.value:", this.value)
 
 }
 
 // func.call(foo)
+ 
 
-
-
+// 第一步
 Function.prototype.call2 = function(context) {
   context.fn = this
   context.fn()
@@ -21,5 +25,16 @@ Function.prototype.call2 = function(context) {
 
 func.call2(foo)
 // foo
-console.log("🚀 ~ file: call.js:24 ~ foo:", foo)
+// console.log("🚀 ~ file: call.js:24 ~ foo:", foo)
+
+// 第二步
+Function.prototype.call2 = function(context, ...args) {
+  context.fn = this || window
+  const result = context.fn(...args)
+  delete context.fn
+
+  return result
+}
+
+func.call2(foo, 1, 2)
 
